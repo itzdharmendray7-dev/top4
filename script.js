312dec0aa42f2f934fe7d0a3ca31f57734ddf508
itzdharmendray7-dev <76218092+itzdharmendray7-dev@users.noreply.github.com>
2021-12-06 10:19:13 +0530

    init

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const data = {
-  mainTitle: "My Favourite Travel Destinations",
+  mainTitle: "My Favourite Nature Photos",
   cardContents: [
     {
-      name: "Kerala",
+      name: "Rain Falling Stars",
       imageLink:
-        "https://images.unsplash.com/photo-1602216056096-3b40cc0c9944?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+        "https://images.unsplash.com/photo-1638091990389-7f4d9bbe8d81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     },
     {
-      name: "Hampi",
+      name: "Flower",
       imageLink:
-        "https://images.unsplash.com/photo-1600100397608-f010f423b971?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80",
+        "https://images.unsplash.com/photo-1638035966446-b2954fdf3525?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1390&q=80",
     },
     {
-      name: "Port Louis",
+      name: "Galaxy",
       imageLink:
-        "https://images.unsplash.com/photo-1581953636842-74649fd3e004?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+        "https://images.unsplash.com/photo-1637228852417-b0da677c1422?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDk4fDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     },
     {
-      name: "Zurich",
+      name: "Desert",
       imageLink:
-        "https://images.unsplash.com/photo-1515488764276-beab7607c1e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=850&q=80",
+        "https://images.unsplash.com/photo-1637493393334-d5cb18846898?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEwMXw2c01WalRMU2tlUXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
     },
   ],
   userData: { name: "Crio.Do", tagLine: "Baby steps into the world of web!" },
